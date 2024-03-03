abf4616b379ffa1a9ae37c99c7250d3012db310f
Uranchimeg MunkhErdene <136231506+AnyaMunkh@users.noreply.github.com>
2024-03-03 22:59:20 +0800

    Update SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,5 +1,5 @@
- -- Fetching only the state name from the full_address column and adding that colum to the table.
-    -- So that i can make the map visualization for each state.   
+-- Fetching only the state name from the full_address column and adding that colum to the table.
+-- So that I can make the map visualization for each state.   
 SELECT 
     full_address,
     SUBSTRING(full_address, CHARINDEX(',', full_address, CHARINDEX(',', full_address) + 1) + 1, 
@@ -13,9 +13,9 @@ UPDATE [Restaurants].[dbo].[US_Merchant - sheet1]
 SET state_name = TRIM(SUBSTRING(full_address, CHARINDEX(',', full_address, CHARINDEX(',', full_address) + 1) + 1, 
 CHARINDEX(',', full_address, CHARINDEX(',', full_address, CHARINDEX(',', full_address) + 1) + 1) - CHARINDEX(',', full_address, 
 CHARINDEX(',', full_address) + 1) - 1))
-
-   --Fetching only the first or word or phrase from the category column, so it can be narrowed down for better analysis.
-   SELECT category, 
+ 
+ --Fetching only the first word or phrase from the category column, so it can be narrowed down for better analysis.
+ SELECT category, 
     CASE WHEN CHARINDEX(',', category) > 0 THEN SUBSTRING(category, 1, CHARINDEX(',', category) - 1) 
     ELSE category 
     END as new_category
@@ -28,7 +28,7 @@ SET new_category = CASE WHEN CHARINDEX(',', category) > 0 THEN SUBSTRING(categor
 ELSE category 
 END
 
--- -- Fetching the city name from the full_address column. So that i can make an visualization only for a specific city. 
+-- Fetching the city name from the full_address column. So that i can make an visualization only for a specific city. 
 SELECT full_address, SUBSTRING(full_address, CHARINDEX(',', full_address) + 2, CHARINDEX(',', full_address, 
 CHARINDEX(',', full_address) + 1) - CHARINDEX(',', full_address) - 2) AS city_name
 FROM [Restaurants].[dbo].[US_Merchant - sheet1]
@@ -42,8 +42,8 @@ CHARINDEX(',', full_address) + 1) - CHARINDEX(',', full_address) - 2)
 
 
 -- Getting a table for a top 10 francises visualization. 
-  Select top(10) restaurant_name, Count(restaurant_name) as Total_Stores, Round(AVG(rate_avg),2) as average_rate
-   FROM [Restaurants].[dbo].[US_Merchant - sheet1]
-   GROUP BY restaurant_name
-   Order BY Total_Stores DESC
+Select top(10) restaurant_name, Count(restaurant_name) as Total_Stores, Round(AVG(rate_avg),2) as average_rate
+FROM [Restaurants].[dbo].[US_Merchant - sheet1]
+GROUP BY restaurant_name
+ORDER BY Total_Stores DESC
 
